0ae74fd4c48f8d70f597356695aa2477b294cd41
szalajm <49290577+szalajm@users.noreply.github.com>
2019-09-10 21:08:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ $(document).ready(function() {
 
     $.ajax({
       url: requestUrl + '/?' + $.param({
-        taskId: taskId
+        id: taskId
       }),
       method: 'DELETE',
       success: function() {
