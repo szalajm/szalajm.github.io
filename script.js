65dd6932b683c2b9a923de676075965339585f32
szalajm <szalajm.neo@gmail.com>
2019-10-20 11:21:50 +0200

    FrontendUpdate2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
+  const apiRoot = ' https://gentle-bayou-08022.herokuapp.com/v1/task';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
