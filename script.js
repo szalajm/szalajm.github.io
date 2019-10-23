7993eb3d7abd9b86f3ca221a7e6baafe947b86e8
szalajm <49290577+szalajm@users.noreply.github.com>
2019-10-23 21:13:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://gentle-bayou-08022.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://gentle-bayou-08022.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
